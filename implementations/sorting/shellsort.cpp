6db86448e2c56bdd384ea09bd33e2b077bb7bddf
matt_hiscocks <mattdhiscocks@gmail.com>
2022-02-14 17:44:33 +0000

    del shellsort

--- implementations/sorting/shellsort.cpp
+++ implementations/sorting/shellsort.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
 
-void shellSort(){
 
+void shellSort(int *arr, int len){
+	int order[3] = {1};
+	
+	for (int &step : order){
+		for (int i=0; i<len-step; i += step){
+		};
+	};	
 };
 
 
 
 
 int main(){
-	
+	arr[5] = {2, 5, 3, 1, 4};
+	shellSort(arr, len);	
 	return 0;
 };
